d665452b3e722a11c3bcc59466c6a6be77595483
tigran1997 <hambardzumyan.tigr@gmail.com>
2018-07-23 22:36:24 +0400

    added README.md and did some changes

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # battle
 php-cli game
+# Instructions
+	Open command line type ```php battle.php SSTHHS SSSTTT```
+
+## Workflow
+	Firstly it gets Army units from command line and pushes it to array of each army unit. (The count of each army units are not always equal, One army can be dominant by count of army units to oponent army unit.)
+
+## The steps of Iteration
+	The battle starts the First army's first unit and then it lasts until one of the army's unites is equal to zero.
+
+Above can be added as much units as you want.
+
+ ```
+ $armyPower = [
+	'T' =>[
+		'health' => 20,
+		'power' => 10,
+	],
+	'H' =>[
+		'health' => 5,
+		'power' => 4,
+	],
+	'S' =>[
+		'health' => 2,
+		'power' => 3,
+	]
+ ]
+ ```
+
+Next code could be converted to partially Object oriented.
+	The Army units can be not an assosiativearrays but array of obects with members and methods health,power, fire or attack which makes code more solid.
+
--- battle.php
+++ battle.php
@@ -1,5 +1,6 @@
-<?php 
-error_reporting(0);
+<?php 	
+error_reporting(0); # turned off for PHP Notices
+
 $armyPower = [
 	'T' =>[
 		'health' => 20,
@@ -14,21 +15,29 @@ $armyPower = [
 		'power' => 3,
 	]
 ];
-// $armyPriorities=[1=>'T',2=>'H', 3=> 'S' ];
+
 $firstArmy=[];
 $secondArmy=[];
+try {
 
-if($argc > 3)
-	echo "Error: Wrong Number of arguments".PHP_EOL;
+  if($argc > 3)
+
+    throw new Exception("Error: Wrong Number of arguments".PHP_EOL."Use like `php battle.php SSTHHS SSSTTT`");
+  
+}
+ catch(Exception $e) {
+  echo 'Message: ' .$e->getMessage();
+  exit;
+}
+
+
 
 
 for ($i=0; $i < strlen($argv[1]); $i++) { 
-		$firstArmyUnits[$argv[1][$i]]++;
 		array_push($firstArmy,$armyPower[$argv[1][$i]] );
 }
 
 for ($i=0; $i < strlen($argv[2]); $i++) { 
-		$secondArmyUnits[$argv[2][$i]]++;	
 		array_push($secondArmy,$armyPower[$argv[2][$i]] );
 }
 
@@ -38,28 +47,28 @@ while (!count($firstArmy) ==0 && !count($secondArmy) == 0) {
 
 	if($firstArmy[$faiteration]["power"] >= $secondArmy[$saiteration]["health"]) { 
 		unset($secondArmy[$saiteration]);
-		print("firstArmy attacks secondArmy losses unit"); echo "\n"; ++$saiteration;
+		print("firstArmy attacks second Army losses unit"); echo "\n"; ++$saiteration;
 	} elseif ($firstArmy[$faiteration]["power"] < $secondArmy[$saiteration]["health"] ) {
-		print("firstArmy attacks secondArmy losses unit health"); echo "\n";
+		print("firstArmy attacks second Army losses unit health"); echo "\n";
 		$secondArmy[$saiteration]["health"] -= $firstArmy[$faiteration]["power"];
 	}
 	
 	if($secondArmy[$saiteration]["power"] >= $firstArmy[$faiteration]["health"]) { 
 		unset($firstArmy[$faiteration]);
-		print("secondArmy attacks  firstArmy losses unit"); echo "\n"; ++$faiteration;
+		print("secondArmy attacks  first Army losses unit"); echo "\n"; ++$faiteration;
 	} elseif ($secondArmy[$saiteration]["power"] < $firstArmy[$faiteration]["health"] ) {
-		print("secondArmy attacks  firstArmy losses unit health"); echo "\n";
+		print("secondArmy attacks  first Army losses unit health"); echo "\n";
 		$firstArmy[$faiteration]["health"] -= $secondArmy[$saiteration]["power"];
 	}
 	
 }
 
-var_dump($firstArmy,$secondArmy);
+// var_dump($firstArmy,$secondArmy); //This can be turned on for visual array structure.
 
 if(count($firstArmy) > 0) {
-	echo "First Army Wins";
+	echo " \n First Army Wins";
 } else {
-	echo "Second Army Wins";
+	echo "\n Second Army Wins";
 }
 
 
