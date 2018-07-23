2e6871c3386e75badefbf53b2a38feb61049dc47
tigran1997 <hambardzumyan.tigr@gmail.com>
2018-07-23 13:06:22 +0400

    cleared code

--- battle.php
+++ battle.php
@@ -1,54 +1,5 @@
 <?php 
 error_reporting(0);
-
-
-class Battle
-{
-	
-	function __construct()
-	{
-		
-	}
-}
-
-
-/**
-* 
-*/
-class Tank
-{
-	public $health = 20;
-	public $power = 10;
-	function __construct()
-	{
-		# code...
-	}
-}
-
-
-class Heavy
-{
-	public $health = 5;
-	public $power = 4;
-	function __construct()
-	{
-		# code...
-	}
-}
-class Soldier
-{
-	public $health = 2;
-	public $power = 3;
-	function __construct()
-	{
-		# code...
-	}
-}
-
-
-$tank = [20=>10];
-$havy = [5=>4];
-$soldier = [2=>3];
 $armyPower = [
 	'T' =>[
 		'health' => 20,
@@ -63,17 +14,14 @@ $armyPower = [
 		'power' => 3,
 	]
 ];
-$armyPriorities=[1=>'T',2=>'H', 3=> 'S' ];
-$firstArmyUnits=[];
-$secondArmyUnits=[];
+// $armyPriorities=[1=>'T',2=>'H', 3=> 'S' ];
 $firstArmy=[];
 $secondArmy=[];
 
+if($argc > 3)
+	echo "Error: Wrong Number of arguments".PHP_EOL;
 
-// echo $argv[2][0];
-// echo $argc;
-// echo $argv[1]; die;
-// echo  count($argv[2]); die;
+
 for ($i=0; $i < strlen($argv[1]); $i++) { 
 		$firstArmyUnits[$argv[1][$i]]++;
 		array_push($firstArmy,$armyPower[$argv[1][$i]] );
@@ -83,33 +31,17 @@ for ($i=0; $i < strlen($argv[2]); $i++) {
 		$secondArmyUnits[$argv[2][$i]]++;	
 		array_push($secondArmy,$armyPower[$argv[2][$i]] );
 }
-// echo min(array_keys($armyPriorities));
-// echo $firstArmyUnits[$armyPriorities[min(array_keys($armyPriorities))]];
 
-// function getHighestInArmy($army,$armyPriorities){
-
-// }
-// var_dump($firstArmyUnits);die;
-// echo "\n 444444444444 \n"; //echo count($secondArmyUnits);
-// var_dump (count($firstArmy) >0 , count($secondArmy) >0); die;
-	$faiteration = 0;
-	$saiteration = 0;
+$faiteration = 0;
+$saiteration = 0;
 while (!count($firstArmy) ==0 && !count($secondArmy) == 0) {
 
-	// echo $firstArmy[$faiteration]["power"];
-	$iter = 0;
 	if($firstArmy[$faiteration]["power"] >= $secondArmy[$saiteration]["health"]) { 
 		unset($secondArmy[$saiteration]);
 		print("firstArmy attacks secondArmy losses unit"); echo "\n"; ++$saiteration;
 	} elseif ($firstArmy[$faiteration]["power"] < $secondArmy[$saiteration]["health"] ) {
 		print("firstArmy attacks secondArmy losses unit health"); echo "\n";
 		$secondArmy[$saiteration]["health"] -= $firstArmy[$faiteration]["power"];
-		// if ($secondArmy[$saiteration]["health"] <=0) {
-		// 	echo "1111111" ; die;
-		// 	echo "during attach secondArmy looses unit"; echo "\n";
-		// 	unset($secondArmy[$saiteration]);
-		// 	++$saiteration;
-		// }
 	}
 	
 	if($secondArmy[$saiteration]["power"] >= $firstArmy[$faiteration]["health"]) { 
@@ -118,17 +50,9 @@ while (!count($firstArmy) ==0 && !count($secondArmy) == 0) {
 	} elseif ($secondArmy[$saiteration]["power"] < $firstArmy[$faiteration]["health"] ) {
 		print("secondArmy attacks  firstArmy losses unit health"); echo "\n";
 		$firstArmy[$faiteration]["health"] -= $secondArmy[$saiteration]["power"];
-		// if ($firstArmy[$faiteration]["health"] <=0) {
-		// 	echo "during attach firstArmy looses unit"; echo "\n";
-		// 	unset($firstArmy[$faiteration]);
-		// 	++$faiteration;
-		// }
-
 	}
-	var_dump($firstArmy,$secondArmy);
 	
 }
-echo "end \n \n";
 
 var_dump($firstArmy,$secondArmy);
 
